d761c314fe03986211b2ec18cff15579b1a80844
Abhash Kumar <100320831+AbhashK1@users.noreply.github.com>
2023-02-09 22:46:50 +0530

    Update flask_test.py

--- flask_test.py
+++ flask_test.py
@@ -9,11 +9,11 @@ text1 = 'Hello, good day to you'
 def english_to_french():
   url='https://api.jp-tok.language-translator.watson.cloud.ibm.com/instances/343971cd-82b8-45ed-a65e-544b18b97226'
   apikey='Us2a4GDOlyzuuLWnfnnGsyZq2z2Ev-tks3wbQlrfvreF'
-  text1 = 'Bonjour, bonne journée à vous'
+  text1 = 'Hello, good day to you'
   authenticator = IAMAuthenticator(apikey)
   language_translator = LanguageTranslatorV3(version='2018-05-01',authenticator=authenticator)
   language_translator.set_service_url(url)
-  frenchtranslation = language_translator.translate(text=text1,model_id='fr-en').get_result()
+  frenchtranslation = language_translator.translate(text=text1,model_id='en-fr').get_result()
   return frenchtranslation.get("translations")[0].get("translation")
 
 if __name__ == "__main__":
